717f1907bea9caeab9d372f3a82611b8565840db
IT24103291 <IT24103291@my.sliit.lk>
2026-09-20 13:21:53 +0530

    Add files via upload

---
+++ Q1A/IT24103291Lab3Q1A.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class IT24103291Lab3Q1A{
+	public static void main(String []args){
+
+	Scanner input =new Scanner (System.in); 
+
+	System.out.println("Enter the price of 1kg of rice: ");
+	double price = input.nextDouble();
+
+	System.out.print("Enter the number of kilograms you want to buy: ");
+        double kilograms = input.nextDouble();
+
+	double amount = price * kilograms;
+	System.out.println("Amount you have to pay: Rs. " + amount);
+		
+  	input.close();
+
+	}
+}
